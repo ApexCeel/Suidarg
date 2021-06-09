c3047f21b6a234f0ceb8d42fc8a1b068e4379b5c
Jefferson Walter Turnbull <Jefferson.Turnbull@gmail.com>
2021-06-09 14:47:16 -0600

    Axis movement and movement bounds.

--- Assets/_Scripts/Player.cs
+++ Assets/_Scripts/Player.cs
@@ -1,38 +1,48 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Security.Principal;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
     [SerializeField] private int speed = 5;
+    [SerializeField] private float horizontalInput;
+    [SerializeField] private float verticalInput;
+
     
     
     void Start()
     {
+        
         transform.position = new Vector3(0, -3.0f);
+        
     }
 
 
     void Update()
     {
-        if (Input.GetKey(KeyCode.A))
-        {
-            transform.Translate(Vector3.left * speed * Time.deltaTime);
-        }
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
 
-        if (Input.GetKey(KeyCode.D))
-        {
-            transform.Translate(Vector3.right * speed * Time.deltaTime);
-        }
+        var direction = new Vector3(horizontalInput, verticalInput, 0);
+        
+        transform.Translate(direction * (speed * Time.deltaTime));
 
-        if (Input.GetKey(KeyCode.W))
+        if (transform.position.x > 9.0f)
         {
-            transform.Translate(Vector3.up * speed * Time.deltaTime);
+            transform.position = new Vector3(9.0f, transform.position.y, transform.position.z);
         }
-
-        if (Input.GetKey(KeyCode.S))
+        if (transform.position.x < -9.0f)
         {
-            transform.Translate(Vector3.down * speed * Time.deltaTime);
+            transform.position = new Vector3(-9.0f, transform.position.y, transform.position.z);
+        }
+        if (transform.position.y > 0)
+        {
+            transform.position = new Vector3(transform.position.x, 0, transform.position.z);
+        }
+        if (transform.position.y < -4)
+        {
+            transform.position = new Vector3(transform.position.x, -4, transform.position.z);
         }
     }
 }
